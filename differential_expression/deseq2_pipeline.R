1d3ade3b8e4329f9fb6723e3cf3909a1928169fb
Adrian de Lomana <alomana@systemsbiology.org>
2020-03-13 14:03:56 -0700

    working on pipeline

--- differential_expression/deseq2_pipeline.R
+++ differential_expression/deseq2_pipeline.R
@@ -1,6 +1,6 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(c("DESeq2", "tximport", "EnsDb.Hsapiens.v86"))
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install(c("DESeq2", "tximport", "EnsDb.Hsapiens.v86"))
 
 library('DESeq2')
 library("tximport")
@@ -11,24 +11,26 @@ setwd("~/scratch/")
 kallisto_dir = "/Volumes/sand/vigur/data/kallisto_shared_folders"
 metadata_file = '/Volumes/sand/vigur/data/metadata.txt'
 
-
-
-# 1. read metadata
-endo_metadata = read.table(metadata_file, header = TRUE)
-endo_metadata
-
-
-
-##### annotation
-txdb <- EnsDb.Hsapiens.v86
-k <- keys(txdb, keytype = "TXNAME")
-tx2gene <- select(txdb, k, "GENEID", "TXNAME")
+# 1. build annotation reference
+txdb = EnsDb.Hsapiens.v86
+k = keys(txdb, keytype = "TXNAME")
+tx2gene = select(txdb, k, "GENEID", "TXNAME")
 tx2geje
 
-# 2. read kallisto files
-files = file.path(kallisto_dir, endo_metadata$run, "abundance.tsv")
-files
+# 2. read metadata
+endo_metadata = read.table(metadata_file, header = TRUE)
+endo_metadata$condition <- factor(rep(c("A","B"),each=3))
+rownames(endo_metadata) <- endo_metadata$run
+endo_metadata
 
-# 2. 
-txi = tximport(files, type="salmon", tx2gene=tx2gene)
-ddsTxi = DESeqDataSetFromTximport(txi, colData=samples, design = ~ condition)
+# 2. define kallisto files
+files = file.path(kallisto_dir, endo_metadata$run, "abundance.h5")
+files
+names(files) <- paste0("sample", 1:6)
+
+# 3. read kallisto files 
+txi = tximport(files, type="kallisto", tx2gene=tx2gene, ignoreAfterBar=TRUE, ignoreTxVersion=TRUE)
+head(txi$counts)
+
+# analysis
+ddsTxi = DESeqDataSetFromTximport(txi, colData=metadata, design=~condition)
