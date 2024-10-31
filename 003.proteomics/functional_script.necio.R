cd6f733833e5752b3d204f9162aa704cacd3e2e4
Adrián López García de Lomana <14022465+adelomana@users.noreply.github.com>
2024-10-31 15:45:32 +0000

    tentative submission from necio next day

---
+++ 003.proteomics/functional_script.necio.R
@@ -0,0 +1,74 @@
+#if (!require("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+
+#BiocManager::install("clusterProfiler")
+#BiocManager::install("org.Hs.eg.db")
+#BiocManager::install("ReactomePA")
+#BiocManager::install("tictoc")
+#BiocManager::install("wesanderson")
+
+#
+# 0. load libraries
+#
+library(crayon)
+library(clusterProfiler)
+library(enrichplot)
+library(tictoc)
+library(viridis)
+library(ggplot2)
+
+#
+# 2. read files and generate lists of genes
+#
+filename = 'deps.04.blue.tsv'
+df = read.table(filename, sep='\t', header=TRUE)
+ensemblIDs = df$ENSEMBL
+deps04blue = bitr(ensemblIDs, fromType = 'ENSEMBL', toType = 'ENTREZID', OrgDb='org.Hs.eg.db')
+dim(deps04blue)
+
+filename = 'deps.04.red.tsv'
+df = read.table(filename, sep='\t', header=TRUE)
+ensemblIDs = df$ENSEMBL
+deps04red = bitr(ensemblIDs, fromType = 'ENSEMBL', toType = 'ENTREZID', OrgDb='org.Hs.eg.db')
+dim(deps04red)
+
+filename = 'deps.24.blue.tsv'
+df = read.table(filename, sep='\t', header=TRUE)
+ensemblIDs = df$ENSEMBL
+deps24blue = bitr(ensemblIDs, fromType = 'ENSEMBL', toType = 'ENTREZID', OrgDb='org.Hs.eg.db')
+dim(deps24blue)
+
+filename = 'deps.24.red.tsv'
+df = read.table(filename, sep='\t', header=TRUE)
+ensemblIDs = df$ENSEMBL
+deps24red = bitr(ensemblIDs, fromType = 'ENSEMBL', toType = 'ENTREZID', OrgDb='org.Hs.eg.db')
+dim(deps24red)
+
+geneLists = list('DEPs t = 4 h down'=deps04blue$ENTREZID, 
+                 'DEPs t = 4 h up'=deps04red$ENTREZID, 
+                 'DEPs t = 24 h down'=deps24blue$ENTREZID,
+                 'DEPs t = 24 h up'=deps24red$ENTREZID
+                )
+
+#
+# 3. run the analysis on different Ontologies
+#
+# this step takes surprisingly long time---
+significance_threshold = 0.05
+tic()
+ck = compareCluster(geneLists, 
+                    fun="enrichPathway", 
+                    pvalueCutoff=significance_threshold)
+toc()
+
+p1 = dotplot(ck, size='count', showCategory=15, font.size=8) 
+print(p1)
+
+my_log_breaks = round(log10(significance_threshold)):round(log10(min(ck@compareClusterResult$p.adjust)))
+my_log_breaks = my_log_breaks[ my_log_breaks %% 2 == 0] 
+my_breaks = 10**my_log_breaks
+p5 = p1 +  scale_fill_viridis(direction=-1, trans="log", breaks=my_breaks, option='cividis')
+print(p5)
+
+storage_file = 'clusterProfiler_enrichments.tsv'
+write.table(ck@compareClusterResult, storage_file, quote=FALSE, sep='\t')
