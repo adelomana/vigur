2f91d57f8db3df5fe9c70e67f027639162dede93
Adrián López García de Lomana <14022465+adelomana@users.noreply.github.com>
2024-11-08 18:55:14 +0000

    large update on functional analysis

--- 003.proteomics/functional_script.R
+++ 003.proteomics/functional_script.R
@@ -19,27 +19,43 @@ library(ggplot2)
 #
 # 2. read files and generate lists of genes
 #
-filename = 'deps.04.blue.tsv'
-df = read.table(filename, sep='\t', header=TRUE)
-ensemblIDs = df$ENSEMBL
+filename = 'colored_proteins_04.tsv' 
+df = read.csv(filename, sep='\t', header=TRUE)
+subset = df[df$Color_in_plot == 'blue', ]
+ensemblIDs = unique(unlist(strsplit(subset$ENSEMBL, ",")))
+print(dim(df))
+print(dim(subset))
+print(length(ensemblIDs))
 deps04blue = bitr(ensemblIDs, fromType = 'ENSEMBL', toType = 'ENTREZID', OrgDb='org.Hs.eg.db')
 dim(deps04blue)
 
-filename = 'deps.04.red.tsv'
-df = read.table(filename, sep='\t', header=TRUE)
-ensemblIDs = df$ENSEMBL
+filename = 'colored_proteins_04.tsv' 
+df = read.csv(filename, sep='\t', header=TRUE)
+subset = df[df$Color_in_plot == 'red', ]
+ensemblIDs = unique(unlist(strsplit(subset$ENSEMBL, ",")))
+print(dim(df))
+print(dim(subset))
+print(length(ensemblIDs))
 deps04red = bitr(ensemblIDs, fromType = 'ENSEMBL', toType = 'ENTREZID', OrgDb='org.Hs.eg.db')
 dim(deps04red)
 
-filename = 'deps.24.blue.tsv'
-df = read.table(filename, sep='\t', header=TRUE)
-ensemblIDs = df$ENSEMBL
+filename = 'colored_proteins_24.tsv' 
+df = read.csv(filename, sep='\t', header=TRUE)
+subset = df[df$Color_in_plot == 'blue', ]
+ensemblIDs = unique(unlist(strsplit(subset$ENSEMBL, ",")))
+print(dim(df))
+print(dim(subset))
+print(length(ensemblIDs))
 deps24blue = bitr(ensemblIDs, fromType = 'ENSEMBL', toType = 'ENTREZID', OrgDb='org.Hs.eg.db')
 dim(deps24blue)
 
-filename = 'deps.24.red.tsv'
-df = read.table(filename, sep='\t', header=TRUE)
-ensemblIDs = df$ENSEMBL
+filename = 'colored_proteins_24.tsv' 
+df = read.csv(filename, sep='\t', header=TRUE)
+subset = df[df$Color_in_plot == 'red', ]
+ensemblIDs = unique(unlist(strsplit(subset$ENSEMBL, ",")))
+print(dim(df))
+print(dim(subset))
+print(length(ensemblIDs))
 deps24red = bitr(ensemblIDs, fromType = 'ENSEMBL', toType = 'ENTREZID', OrgDb='org.Hs.eg.db')
 dim(deps24red)
 
@@ -60,22 +76,26 @@ geneLists = list(
 # this step takes surprisingly long time---
 significance_threshold = 0.05
 tic()
-ck = compareCluster(geneLists, 
-                    fun="enrichPathway", 
-                    pvalueCutoff=significance_threshold)
+#ck = compareCluster(geneLists, fun="enrichPathway", pvalueCutoff=significance_threshold)
+#ck = compareCluster(geneLists, fun='enrichGO', OrgDb='org.Hs.eg.db', pvalueCutoff=significance_threshold)
+ck = compareCluster(geneLists, fun='enrichKEGG', pvalueCutoff=significance_threshold)
 toc()
+#cks = simplify(ck)
+cks=ck
 
-p1 = dotplot(ck, size='count', showCategory=20, font.size=6) 
+p1 = dotplot(cks, size='count', showCategory=10, font.size=8) 
 print(p1)
 
 # and I have a preference for cividis, but this is just personal preference
-my_log_breaks = round(log10(significance_threshold)):round(log10(min(ck@compareClusterResult$p.adjust)))
-my_log_breaks = my_log_breaks[ my_log_breaks %% 2 == 0] 
+my_log_breaks = round(log10(significance_threshold)):round(log10(min(cks@compareClusterResult$p.adjust)))
+my_log_breaks = my_log_breaks[ my_log_breaks %% 3 == 0] 
 my_breaks = 10**my_log_breaks
 p5 = p1 +  scale_fill_viridis(direction=-1, trans="log", breaks=my_breaks, option='cividis')
 print(p5)
+ggsave('dotplot.png', dpi=600)
+dev.off()
 # arguably this plot communicates best data patterns, IMHO
 
 # importantly, store your fuctional enrichment in a form of table which will be a supplementary file of your paper
 storage_file = 'clusterProfiler_enrichments.tsv'
-write.table(ck@compareClusterResult, storage_file, quote=FALSE, sep='\t')
+write.table(cks@compareClusterResult, storage_file, quote=FALSE, sep='\t')
